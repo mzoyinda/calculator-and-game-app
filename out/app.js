e74d3009adf92e91c1556bded327a62491ecd1e7
oyindamola <oyindamoladawodu22@gmail.com>
2022-01-06 20:17:13 +0100

    game added

--- out/app.js
+++ out/app.js
@@ -1,4 +1,38 @@
 "use strict";
-var message = "Hello World!";
-console.log(message);
+function leftArrowPressed() {
+    let element = document.getElementById("image1");
+    element.style.left = parseInt(element.style.left) - 40 + 'px';
+}
+function rightArrowPressed() {
+    let element = document.getElementById("image1");
+    element.style.left = parseInt(element.style.left) + 40 + 'px';
+}
+function upArrowPressed() {
+    let element = document.getElementById("image1");
+    element.style.top = parseInt(element.style.top) - 40 + 'px';
+}
+function downArrowPressed() {
+    let element = document.getElementById("image1");
+    element.style.top = parseInt(element.style.top) + 40 + 'px';
+}
+function moveSelection(event) {
+    switch (event.keyCode) {
+        case 37:
+            leftArrowPressed();
+            break;
+        case 39:
+            rightArrowPressed();
+            break;
+        case 38:
+            upArrowPressed();
+            break;
+        case 40:
+            downArrowPressed();
+            break;
+    }
+}
+;
+function gameLoop() {
+    moveSelection();
+}
 //# sourceMappingURL=app.js.map
